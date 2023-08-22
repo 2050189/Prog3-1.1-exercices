a3e000000d8e5605487535662b25bdaf54d9146f
sarah-e hsieh <2050189@cegepmontpetit.ca>
2023-08-22 13:47:24 -0400

    INIT Exercice bonjour java + FCT complétion
    création des deux classes et des print

---
+++ Exercice Bonjour Java/src/main/java/org/hsieh/demo/Hsieh.java
@@ -0,0 +1,15 @@
+package org.hsieh.demo;
+
+/**
+ * Hello world!
+ *
+ */
+
+
+public class Hsieh
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Exercice Bonjour Java/src/main/java/org/hsieh/demo/Mathieu.java
@@ -0,0 +1,2 @@
+package org.hsieh.demo;public class Mathieu {
+}
