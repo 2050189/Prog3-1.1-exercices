0382de35b0719f5514a4a181dc6dc400f8fcb897
sarah-e hsieh <2050189@cegepmontpetit.ca>
2023-08-22 13:34:28 -0400

    FCT exemples

--- Prog3-1.1-projet1/src/main/java/org/hsieh/App.java
+++ Prog3-1.1-projet1/src/main/java/org/hsieh/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int x = 1;
+        System.out.println( "Yo!" );
+        x = 2;
+        System.out.println( "Allo!" );
     }
 }
